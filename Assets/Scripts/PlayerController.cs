49332f25a7c1ade6d9ee5d0095977c1160f44607
Andrew D <retroburst@gmail.com>
2015-04-06 18:04:09 +0900

    Work in progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb = null;
 	public float Speed = 0.0f;
+	private int count = 0;
 
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -19,5 +20,12 @@ public class PlayerController : MonoBehaviour {
 		Vector3 move = new Vector3 (moveHorizontal, 0, moveVertical);
 		rb.AddForce (move * Speed * Time.deltaTime);
 
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive (false);
+			count++;
+		}
 	}
 }
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(Random.Range(0,100), Random.Range(0,100), Random.Range(0,100)) * Time.deltaTime);
+	}
+}
